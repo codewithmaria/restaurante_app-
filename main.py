e5158a9fc8b160aaf2c7ee907bbf3d847eca9043
Maria Oliveira <mariduardasantos@gmail.com>
2026-05-26 22:23:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import os
 import streamlit as st
 
-# mapeia a pasta atual de execução
+# Configuração absoluta de caminhos locais
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import produtos
