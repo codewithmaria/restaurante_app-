1f94395fa44d5db8ce78fb6018af6c83966e3240
Maria Oliveira <mariduardasantos@gmail.com>
2026-05-26 22:35:41 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -1,3 +1,9 @@
-# Módulo de persistência de dados integrado ao session_state do Streamlit
-# Os estados globais são validados no arranque do main.py
-pass
+# Banco de dados inicial em memória (Listas e Dicionários)
+cardapio = [
+    {"codigo": 1, "nome": "Hamburguer Artesanal", "preco": 28.50},
+    {"codigo": 2, "nome": "Batata Frita Media", "preco": 12.00},
+    {"codigo": 3, "nome": "Refrigerante Lata", "preco": 6.00}
+]
+
+# Guarda os dicionários de cada pedido fechado
+historico_pedidos = []
