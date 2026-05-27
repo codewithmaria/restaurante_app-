8b7f8d37fc2c039fb1c33fb0f7c61a2aa1f466ec
Maria Oliveira <mariduardasantos@gmail.com>
2026-05-26 21:57:30 -0300

    Create dados.py

---
+++ dados.py
@@ -0,0 +1,9 @@
+# cardápio incial para realizar teste
+cardapio = [
+    {"codigo": 1, "nome": "Hambúrguer Artesanal", "preco": 28.50},
+    {"codigo": 2, "nome": "Batata Frita Média", "preco": 12.00},
+    {"codigo": 3, "nome": "Refrigerante Lata", "preco": 6.00},
+]
+
+# histórico de pedidos realizados
+historico_pedidos = []
